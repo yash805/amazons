781b8d21771050d04098d0cf68e202ade00fd9b4
yash805 <74845238+yash805@users.noreply.github.com>
2023-11-20 23:31:26 +0530

    Update Maincomp.js

--- src/Components/home/Maincomp.js
+++ src/Components/home/Maincomp.js
@@ -29,7 +29,7 @@ const Maincomp = () => {
         <div className="right_slide">
            <h4>Festive Launches</h4>
            <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/Jupiter/Launches/T3/DesktopGateway_CategoryCard2x_758X608_T3._SY608_CB639883570_.jpg" alt="rightimg" />
-           <a href="#">see More</a>
+          
         </div>
       </div>
       <Slide title="Today's Deal"  products={products} />
